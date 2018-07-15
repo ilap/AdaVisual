a87d17c7ed1a2ac7918a52c30d3057ec6c7b51cd
ilap <pal.dorogi@gmail.com>
2018-07-16 03:50:40 +1000

    Fixed renderLink

--- demos/src/ui/renderLink.js
+++ demos/src/ui/renderLink.js
@@ -1,6 +1,6 @@
 module.exports = function (link, ctx) {
-  ctx.lineStyle(1, 0xFFFFFF, 1);
-  //ctx.lineStyle(link.width, 0xFFFFFF, 1);
+  //ctx.lineStyle(1, 0xFFFFFF, 1);
+  ctx.lineStyle(link.width, 0xFFFFFF, 1);
   ctx.moveTo(link.from.x, link.from.y);
   ctx.lineTo(link.to.x, link.to.y);
 }
