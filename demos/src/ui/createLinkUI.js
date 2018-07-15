25f4939746801d65deebbe19ede3261cfaa1c6e1
ilap <pal.dorogi@gmail.com>
2018-07-16 04:14:39 +1000

    Fully refactored, based on code in demo direcotry.

--- demos/src/ui/createLinkUI.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (link) {
-  return {
-    width: 67 - Math.log(link.toId)
-  }
-};
--- server.js
+++
@@ -1,6 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-
-connect().use(serveStatic(__dirname + '/dist/')).listen(8082, function(){
-    console.log('Server running on 8082...');
-});
